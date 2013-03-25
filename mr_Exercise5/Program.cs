df24d0bf3110f0f1b369d621a2a8fb3c55f858ad
John Slabbekoorn <john.slabbekoorn@gmail.com>
2013-03-25 21:54:21 +0100

    New

---
+++ mr_Exercise5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mr_Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int RIJ = 25;
+            const int KOLOM = 35;
+            char charChars;
+
+            Console.WriteLine();
+
+            Random randNummer = new Random();
+            char[,] charArray = new char[RIJ, KOLOM];
+
+
+            for (int a = 0; a < 50; a++)
+            {
+                for (int i = 0; i < RIJ; i++)
+                {
+                    for (int j = 0; j < KOLOM; j++)
+                    {
+                        charChars = Convert.ToChar(randNummer.Next(126, 255));
+                        charArray[i, j] = charChars;
+                        Console.Write(charArray[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                System.Threading.Thread.Sleep(200);
+                Console.Clear();
+            }
+            Console.ReadLine();
+        }
+    }
+}
